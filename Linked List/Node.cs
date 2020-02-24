dd46303bb55c1b7a19d874ec256158c5748b2348
KostiantynLogunov <777koc@gmail.com>
2020-02-24 15:45:36 +0200

    Linked List was completed

---
+++ Linked List/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ Linked List/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            // добавление элементов
+            linkedList.Add("Tom");
+            linkedList.Add("Alice");
+            linkedList.Add("Bob");
+            linkedList.Add("Sam");
+
+            // выводим элементы
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------------------------------------------");
+
+            // удаляем элемент
+            linkedList.Remove("Alice");
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------------------------------------------");
+
+            // проверяем наличие элемента
+            bool isPresent = linkedList.Contains("Sam");
+            Console.WriteLine(isPresent == true ? "Sam присутствует" : "Sam отсутствует");
+
+            // добавляем элемент в начало            
+            linkedList.AppendFirst("Bill");
+            Console.WriteLine("---------------------------------------------------");
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
