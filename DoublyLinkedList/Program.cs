bde27367a2aa4e844a8f012248c66286f6af8627
KostiantynLogunov <777koc@gmail.com>
2020-02-25 11:28:34 +0200

    DoublyLinkedList was completed

---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            
+            // добавление элементов
+            linkedList.Add("Bob");
+            linkedList.Add("Bill");
+            linkedList.Add("Tom");
+            linkedList.AddFirst("Kate");
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------");
+
+            // удаление
+            linkedList.Remove("Bill");
+
+            // перебор с последнего элемента
+            foreach (var t in linkedList.BackEnumerator())
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
